d710ea7147e6580ca6dfbd74ac95585d31683491
Raul <raul.ruiz.barea@gmail.com>
2018-11-21 20:59:04 +0100

    New user page

--- src/app/pages/new-user/new-user.page.ts
+++ src/app/pages/new-user/new-user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from 'src/app/constants/app.constants';
+import { User } from 'src/app/shared/model/user';
 
 @Component({
   selector: 'app-new-user',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user.page.scss'],
 })
 export class NewUserPage implements OnInit {
+  username: string;
+  password: string;
+  passwordRepeat: string;
+  name: string;
+  surnames: string;
+  email: string;
+  createAccount: string;
 
-  constructor() { }
+  user: User = {
+    name: '',
+    surnames: '',
+    email: '',
+  };
+
+  constructor() {
+    this.username = Constants.Username;
+    this.password = Constants.Password;
+    this.passwordRepeat = Constants.PasswordRepeat;
+    this.name = Constants.Name;
+    this.surnames = Constants.Surnames;
+    this.email = Constants.Email;
+    this.createAccount = Constants.CreateAccount;
+   }
 
   ngOnInit() {
+    console.log('ngOnInit NewUserPage');
   }
 
 }
