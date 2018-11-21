c9922f817a09c4c8670bccb5fa50f4be6c1d7b45
Raul <raul.ruiz.barea@gmail.com>
2018-11-21 22:40:01 +0100

    Clean routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'new-user', loadChildren: './pages/new-user/new-user.module#NewUserPageModule' },
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+  { path: 'home',
+    loadChildren: './pages/home/home.module#HomePageModule',
+  },
+  {
+    path: 'welcome',
+    loadChildren: './pages/welcome/welcome.module#WelcomePageModule',
+  },
+  {
+    path: 'login',
+    loadChildren: './pages/login/login.module#LoginPageModule',
+  },
+  {
+    path: 'new-user',
+    loadChildren: './pages/new-user/new-user.module#NewUserPageModule'
+  },
 ];
 
 @NgModule({
