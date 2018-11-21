9825887803000a7c6cd3bdc9353a041867836f06
Raul <raul.ruiz.barea@gmail.com>
2018-11-21 19:05:25 +0100

    Constants

---
+++ src/app/constants/app.constants.ts
@@ -0,0 +1,55 @@
+export const Constants = {
+  // App
+  AppName: '52sneakers',
+  // Header
+  HeaderTitle: '52sneakers',
+  // Footer
+  FooterTitle: 'Creat per Raul Ruiz per a la UOC',
+  // Welcome
+  Skip: 'Ometre',
+  SliderOneTitle: 'Benvingut a 52 Sneakers!',
+  Continue: 'Continua',
+  ReadyTo: 'Preparat?',
+  SliderOneDescription: 'Text slider 1',
+  SliderTwoTitle: 'Titol 2',
+  SliderTwoDescription: 'Text slider 2',
+  SliderThreeTitle: 'Titol 3',
+  SliderThreeDescription: 'Text slider 3',
+  //  Login
+  Username: 'Usuari',
+  Password: 'Password',
+  PasswordRepeat: 'Repeteix el password',
+  SignIn: 'Iniciar sessió',
+  CreateAccount: 'Crea un compte',
+  ForgetPassword: 'Has oblidat el password?',
+  PasswordReset: 'Reiniciar password',
+  // User
+  Name: 'Nom',
+  Surnames: 'Cognoms',
+  Email: 'Email',
+
+  // Menu
+  Home: 'Home',
+  Menu: 'Menú',
+  News: 'Noticies',
+  Sports: 'Esports',
+  Subscription: 'Subscriu-te',
+  Contact: 'Contacta',
+  Football: 'Fútbol',
+  Running: 'Running',
+  Basketball: 'Basket',
+  Training: 'Training',
+
+  //  Profile
+  Save: 'Desar',
+  Notifications: 'Notificacions',
+  Favourites: 'Favorits',
+  History: 'Historial',
+  Configuration: 'Configuration',
+  Disconnect: 'Desconnectar',
+  Cancel: 'Cancel·lar',
+  Profile: 'Perfil',
+
+  //  Search
+  Search: 'Cerca',
+};
