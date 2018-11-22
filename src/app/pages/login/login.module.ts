ac40112607a9576f150c8fb4dd981aa63191aa89
Raul <raul.ruiz.barea@gmail.com>
2018-11-22 16:10:54 +0100

    Login reactive forms

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
 import { ComponentsModule } from 'src/app/components/components.module';
+
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     ComponentsModule,
     RouterModule.forChild(routes)
