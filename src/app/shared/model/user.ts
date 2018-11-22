192cff60e72ffd1c2ae692a7ccf048e922197e12
Raul <raul.ruiz.barea@gmail.com>
2018-11-22 01:06:56 +0100

    User class

--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -1,6 +1,18 @@
-export interface User {
-  key?: string;
-  name: string;
-  surnames: string;
-  email: string;
+export class User {
+  constructor(
+    public $key: string,
+    public name: string,
+    public surnames: string,
+    public email: string,
+    public password: string) {
+
+  }
+
+  static fromJsonList(array): User[] {
+    return array.map(User.fromJson);
+  }
+
+  static fromJson({$key, name, surnames, email, password}): User {
+    return new User($key, name, surnames, email, password);
+  }
 }
