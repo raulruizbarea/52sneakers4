f9d5ed86fbf6f41af05d701c8b18aecf9a84d644
raulruizbarea <raul.ruiz.barea@gmail.com>
2018-11-23 13:51:20 +0100

    Add resetPassword with Email in AuthService

--- src/app/shared/security/auth.service.ts
+++ src/app/shared/security/auth.service.ts
@@ -41,6 +41,12 @@ export class AuthService {
 
     return subject.asObservable();
   }
+  
+  resetPassword(email) {
+    return this.afAuth.auth.sendPasswordResetEmail(email)
+      .then(() => console.log("Email sent"))
+      .catch((error) => console.log(error))
+  }
 
   logout() {
     this.afAuth.auth.signOut();
