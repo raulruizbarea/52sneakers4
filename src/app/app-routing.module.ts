552bf94f0ce96c61b0a75e30d09545d28ae5ef26
raulruizbarea <raul.ruiz.barea@gmail.com>
2018-11-23 11:25:23 +0100

    AuthGuard on routes
    Add canActivate: [AuthGuard] in the routes you must be logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared/security/auth.guard';
 
 const routes: Routes = [
   {
