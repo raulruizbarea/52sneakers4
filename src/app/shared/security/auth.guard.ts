e7644fcd261ad3fc0ad6235ccfcb14cf32a132d1
Raul <raul.ruiz.barea@gmail.com>
2018-11-24 02:42:23 +0100

    Change guard to save persistence

--- src/app/shared/security/auth.guard.ts
+++ src/app/shared/security/auth.guard.ts
@@ -2,26 +2,31 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import {tap, take, map} from 'rxjs/operators';
+import { AngularFireAuth } from 'angularfire2/auth';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService, private router: Router) {
 
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.authService.authInfo$.pipe(
-        map(authInfo => authInfo.isLoggedIn()),
-        take(1),
-        tap(allowed => {
-            if (!allowed) {
-                this.router.navigate(['/login']);
-            }
-        }), );
-  }
+    state: RouterStateSnapshot): Observable<boolean> {
+
+      return this.afAuth.authState
+       .take(1)
+       .map(user => !!user)
+       .do(loggedIn => {
+         if (!loggedIn) {
+           console.log('access denied');
+           this.router.navigate(['/login']);
+         }
+     });
+   }
 }
