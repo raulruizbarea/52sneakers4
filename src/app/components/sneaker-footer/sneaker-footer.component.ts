fa9b7579b544fa998effea4d5ed72c849ca42379
Raul <raul.ruiz.barea@gmail.com>
2018-11-25 02:16:57 +0100

    Add buttons to footer

--- src/app/components/sneaker-footer/sneaker-footer.component.ts
+++ src/app/components/sneaker-footer/sneaker-footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Constants } from 'src/app/constants/app.constants';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthInfo } from 'src/app/shared/security/auth-info';
+import { AuthService } from 'src/app/shared/security/auth.service';
 
 @Component({
   selector: 'sneaker-footer',
@@ -7,13 +10,18 @@ import { Constants } from 'src/app/constants/app.constants';
   styleUrls: ['./sneaker-footer.component.scss']
 })
 export class SneakerFooterComponent implements OnInit {
+  authInfo: AuthInfo;
   footerTitle: string;
 
-  constructor() {
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService) {
     this.footerTitle = Constants.FooterTitle;
   }
 
   ngOnInit() {
+    this.authService.authInfo$.subscribe(authInfo =>  this.authInfo = authInfo);
   }
 
+  isLoggedIn() {
+    return this.afAuth.auth.currentUser;
+  }
 }
