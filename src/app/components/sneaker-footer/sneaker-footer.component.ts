df3fff01f24e1ee56b6c23a8b5b1f22c45ea3133
Raul <raul.ruiz.barea@gmail.com>
2018-11-29 10:51:05 +0100

    Add links in footer component to profile and filter

--- src/app/components/sneaker-footer/sneaker-footer.component.ts
+++ src/app/components/sneaker-footer/sneaker-footer.component.ts
@@ -3,6 +3,8 @@ import { Constants } from 'src/app/constants/app.constants';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthInfo } from 'src/app/shared/security/auth-info';
 import { AuthService } from 'src/app/shared/security/auth.service';
+import { ModalController, NavController } from '@ionic/angular';
+import { FilterPage } from 'src/app/pages/filter/filter.page';
 
 @Component({
   selector: 'sneaker-footer',
@@ -13,7 +15,8 @@ export class SneakerFooterComponent implements OnInit {
   authInfo: AuthInfo;
   footerTitle: string;
 
-  constructor(private afAuth: AngularFireAuth, private authService: AuthService) {
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService,
+    private modalCtrl: ModalController, private navCtrl: NavController) {
     this.footerTitle = Constants.FooterTitle;
   }
 
@@ -23,5 +26,21 @@ export class SneakerFooterComponent implements OnInit {
 
   isLoggedIn() {
     return this.afAuth.auth.currentUser;
+  }
+
+  navigateToFilter() {
+    this.openModal(FilterPage);
+  }
+
+  navigateToProfile() {
+    this.navCtrl.navigateForward('/profile');
+  }
+
+  async openModal(comp) {
+    const modal = await this.modalCtrl.create({
+     component: comp,
+   });
+
+   return await modal.present();
   }
 }
