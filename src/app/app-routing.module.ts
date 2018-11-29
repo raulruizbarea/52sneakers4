617a4ac180b77f8626779f97d43baaab644d5414
Raul <raul.ruiz.barea@gmail.com>
2018-11-29 10:33:41 +0100

    Protected beginning pages if you are logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/security/auth.guard';
+import { LoginGuard } from './shared/security/login.guard';
 
 const routes: Routes = [
   {
@@ -15,14 +16,17 @@ const routes: Routes = [
   {
     path: 'welcome',
     loadChildren: './pages/welcome/welcome.module#WelcomePageModule',
+    canActivate: [LoginGuard],
   },
   {
     path: 'login',
     loadChildren: './pages/login/login.module#LoginPageModule',
+    canActivate: [LoginGuard],
   },
   {
     path: 'new-user',
-    loadChildren: './pages/new-user/new-user.module#NewUserPageModule'
+    loadChildren: './pages/new-user/new-user.module#NewUserPageModule',
+    canActivate: [LoginGuard],
   },
   {
     path: 'news',
@@ -41,6 +45,7 @@ const routes: Routes = [
   },
   { path: 'forget-password',
     loadChildren: './pages/forget-password/forget-password.module#ForgetPasswordPageModule',
+    canActivate: [LoginGuard],
   },
   {
     path: 'subscription',
---
+++ src/app/shared/security/login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+
+      return this.afAuth.authState
+       .take(1)
+       .map(user => !!user)
+       .do(loggedIn => {
+         if (loggedIn) {
+           console.log('access granted');
+           this.router.navigate(['/home']);
+         }
+     });
+   }
+}
