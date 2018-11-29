875f5988be3b4522d7ad17caee0fd8de1e401814
zxBCN Ruiz <raul.ruiz.ext@boehringer-ingelheim.com>
2018-11-29 15:20:05 +0100

    Handle hide or not menu and right part

--- src/app/components/sneaker-header/sneaker-header.component.ts
+++ src/app/components/sneaker-header/sneaker-header.component.ts
@@ -6,6 +6,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Input } from '@angular/core';
 import { NavController, ModalController } from '@ionic/angular';
 import { SearchPage } from 'src/app/pages/search/search.page';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sneaker-header',
@@ -14,14 +15,18 @@ import { SearchPage } from 'src/app/pages/search/search.page';
 })
 export class SneakerHeaderComponent implements OnInit {
   @Input() appName: string;
+  @Input() hideMenu: boolean;
+  @Input() hideRightMenu: boolean;
   authInfo: AuthInfo;
 
   back: string;
 
   constructor(private authService: AuthService, private afAuth: AngularFireAuth,
-    private navCtrl: NavController, private modalCtrl: ModalController) {
+    private navCtrl: NavController, private modalCtrl: ModalController, private router: Router) {
     this.appName = Constants.AppName;
     this.back = Constants.Back;
+    this.hideMenu = false;
+    this.hideRightMenu = false;
   }
 
   isLoggedIn() {
