935baf3b1ffd5dede90796463bc34d67d3ebdab7
Raul <raul.ruiz.barea@gmail.com>
2018-12-02 02:28:40 +0100

    OrderBy pipe

---
+++ src/app/shared/security/pipes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { OrderByPipe } from './order-by.pipe';
+
+@NgModule({
+  declarations: [OrderByPipe],
+  imports: [],
+  exports: [OrderByPipe]
+})
+export class PipesModule {}
