50cfd5fb8496dc4b0b128aeadeb09434ed1d8da8
Raul <raul.ruiz.barea@gmail.com>
2018-12-02 20:17:28 +0100

    Add select size and add to cart in Sneaker

--- src/app/pages/sneaker/sneaker.page.ts
+++ src/app/pages/sneaker/sneaker.page.ts
@@ -6,19 +6,26 @@ import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { formatDate } from '@angular/common';
+import { Constants } from 'src/app/constants/app.constants';
 
 @Component({
   selector: 'app-sneaker',
   templateUrl: './sneaker.page.html',
   styleUrls: ['./sneaker.page.scss'],
 })
-export class SneakerPage implements OnInit, OnDestroy {
+export class SneakerPage implements OnInit {
   sneaker: Sneaker;
+  addToCart: string;
+  sizeTitle: string;
+  sizes: string[];
+  disableBtn: boolean;
 
   constructor(private route: ActivatedRoute, private menuCtrl: MenuController,
     private afAuth: AngularFireAuth, private sneakerService: SneakerService) {
-    this.menuCtrl.enable(false);
-    console.log(this.route.snapshot.paramMap.get('id'));
+    // console.log(this.route.snapshot.paramMap.get('id'));
+    this.addToCart = Constants.AddToCart;
+    this.sizeTitle = Constants.Size;
+    this.disableBtn = true;
    }
 
   ngOnInit() {
@@ -28,11 +35,9 @@ export class SneakerPage implements OnInit, OnDestroy {
       tap(console.log))
       .subscribe(sneaker => {
          this.sneaker = sneaker;
+         // console.log(this.sneaker.sizes);
+         this.sizes = Object.keys(this.sneaker.sizes);
       });
-  }
-
-  ngOnDestroy() {
-    this.menuCtrl.enable(true);
   }
 
   doLike(key) {
@@ -46,5 +51,12 @@ export class SneakerPage implements OnInit, OnDestroy {
     }
 
     this.sneaker.like = !like;
+  }
+
+  canAdd($event) {
+    // console.log($event.target.value);
+    if ($event.target.value !== undefined && $event.target.value !== '') {
+      this.disableBtn = false;
+    }
   }
 }
