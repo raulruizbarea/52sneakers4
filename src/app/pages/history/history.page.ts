71e43981abd64d260930206b06e82800add2109d
Raul <raul.ruiz.barea@gmail.com>
2018-12-07 01:50:13 +0100

    Preparing history

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Order } from 'src/app/shared/model/order';
+import { Constants } from 'src/app/constants/app.constants';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
+  orders: Order[];
+  noValues: string;
+  history: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private orderService: OrderService, private afAuth: AngularFireAuth) {
+    this.noValues = Constants.NoOrders;
+    this.history = Constants.HistoryList;
   }
 
+  ngOnInit() {
+    this.orderService.findAllOrdersByUserKey(this.afAuth.auth.currentUser.uid)
+    .map(orders => orders.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()))
+    .subscribe(values => {
+      this.orders = values;
+    });
+  }
+
+  doRefresh(refresher) {
+    this.orderService.findAllOrdersByUserKey(this.afAuth.auth.currentUser.uid)
+    .map(orders => orders.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()))
+    .subscribe(values => {
+      this.orders = values;
+      refresher.target.complete();
+    });
+  }
 }
